7efac2ac4b0e88b9b3d878db1a45d8b3e354c6c4
fluke <zh.seva2004@mail.ru>
2025-02-12 18:33:57 +0700

    Add test main file & parser class with no-parsing logic.

---
+++ src/main/java/ArgsParser.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArgsParser {
+    private String outputDir = "";
+    private String prefix = "";
+    private boolean append = false;
+    private StatsType statsType = StatsType.NONE;
+    private final List<String> inputFiles = new ArrayList<>();
+
+}
